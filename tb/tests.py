719b699388bf802e46d38a59e0f6595b2bfdb313
Sahas <munamalasahas@gmail.com>
2023-10-09 15:45:30 -0700

    refactor test infrastructure

--- tb/tests.py
+++ tb/tests.py
@@ -8,7 +8,7 @@ import pytest
 from cocotb_test.simulator import run
 
 curdir = os.path.curdir
-uart_vsource_dir = os.path.join(curdir, '../../rtl/uart')
+uart_vsource_dir = os.path.join(curdir, '../rtl/uart')
 
 
 def test_uart_rx():
@@ -16,7 +16,7 @@ def test_uart_rx():
         verilog_sources=[os.path.join(uart_vsource_dir, 'uart_rx.v')],
         toplevel='uart_rx',
         toplevel_lang='verilog',
-        module='test_uart_rx',
+        module='uart_tests.test_uart_rx',
         sim_build='uart_rx_sim',
         timescale='1ns/1ns', # ns
         waves=True,
@@ -27,7 +27,7 @@ def test_uart_tx():
         verilog_sources=[os.path.join(uart_vsource_dir, 'uart_tx.v')],
         toplevel='uart_tx',
         toplevel_lang='verilog',
-        module='test_uart_tx',
+        module='uart_tests.test_uart_tx',
         sim_build='uart_tx_sim',
         timescale='1ns/1ns',
         waves=True,
